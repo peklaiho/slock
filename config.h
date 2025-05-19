4fcf0eae46a9bccda87e1946f6839e91eb2bf34e
Pekka Laiho <pekka.i.laiho@gmail.com>
2025-05-19 17:54:03 +0300

    Use red color for failed login attempt

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const char *group = "nobody";
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
 	[INPUT] =  "#002b36",   /* during input */
-	[FAILED] = "#839496",   /* wrong password */
+	[FAILED] = "#640000",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
